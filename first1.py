4daa920cdbed733b01360967a7a26b9d81c3dace
Zohaib <zohaib.khalid0042@gmail.com>
2021-12-20 17:31:46 +0500

    ksdskd

--- first1.py
+++ first1.py
@@ -15,18 +15,7 @@ for c in encrypted_text:
 print("Original Text : " + encrypted_text)
 
 
-s = 3
-encrypted_text = "WLEXFY"
-plain_text = ""
-for c in encrypted_text:
-    if c.isupper():
-        c_unicode = ord(c)
-        c_index = ord(c) - ord("A")
-        new_index = (c_index + s) % 26
-        new_unicode = new_index + ord("A")
-        new_character = chr(new_unicode)
-        plain_text = plain_text + new_character
-    else:
+
         plain_text += c
 
 print("Encrypted Text : " + encrypted_text)
